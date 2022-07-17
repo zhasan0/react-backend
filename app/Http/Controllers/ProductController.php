e5867303ff4c8b294c77d84b83c05dd7bb41570b
Zahid Hasan <shuvo017637@gmail.com>
2022-07-17 14:10:56 +0600

    edit and delete added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return Product::all();
+        return response()->json(Product::all());
     }
 
     /**
@@ -30,7 +30,7 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -38,8 +38,8 @@ class ProductController extends Controller
         $product = new Product();
         $product->name = $request->name;
         $product->price = $request->price;
-        $product->description = $request->des;
-        $product->file_path = $request->file('file')->store('products');
+        $product->description = $request->description;
+//        $product->file_path = $request->file('file')->store('products');
         $product->save();
 
         return $product;
@@ -48,52 +48,62 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Product  $product
+     * @param \App\Models\Product $product
      * @return \Illuminate\Http\Response
      */
     public function show(Product $product)
     {
-        //
+
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Product  $product
+     * @param \App\Models\Product $product
      * @return \Illuminate\Http\Response
      */
-    public function edit(Product $product)
+    public function edit(Product $product, $id)
     {
-        //
+        $product = Product::find($id);
+
+        return $product;
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Product  $product
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Product $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request, Product $product, $id)
     {
-        //
+        $product = Product::find($id);
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+
+        if ($product->update()){
+            return response()->json('Updated Successfully', 200);
+        }
+        return response()->json('Something went wrong', 500);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Product  $product
+     * @param \App\Models\Product $product
      * @return \Illuminate\Http\Response
      */
     public function destroy(Product $product, $id)
     {
         $result = Product::destroy($id);
-        if ($result){
+        if ($result) {
             return [
                 'msg' => "Product has been deleted",
                 'code' => "200"
             ];
-        }else{
+        } else {
             return [
                 'msg' => "Something went wrong",
                 'code' => "500"
--- routes/api.php
+++ routes/api.php
@@ -24,5 +24,7 @@ Route::post('/register', [UserController::class, 'register'])->name('register');
 Route::post('/login', [UserController::class, 'login'])->name('login');
 
 Route::get('/productList', [ProductController::class, 'index'])->name('productList');
-Route::post('/addProduct', [ProductController::class, 'store'])->name('addProduct');
+Route::any('/addProduct', [ProductController::class, 'store'])->name('addProduct');
+Route::get('/editProduct/{id}', [ProductController::class, 'edit'])->name('editProduct');
+Route::any('/updateProduct/{id}', [ProductController::class, 'update'])->name('updateProduct');
 Route::delete('/productDelete/{id}', [ProductController::class, 'destroy'])->name('deleteProduct');
